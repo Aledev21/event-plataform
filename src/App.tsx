e48ea2ab8a846b95ce00aef00083720627eaa27e
Alessandro Assis <aledev21@gmail.com>
2022-06-27 18:21:35 -0300

    config api

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,26 @@
+import { gql } from "@apollo/client";
+import { useEffect } from "react";
+import { client } from "./lib/apollo";
+
+const GET_LESSEONS_QUERY = gql`
+  query {
+    lessons {
+      id
+      title
+    }
+  }
+`;
+
 function App() {
+  useEffect(() => {
+    client
+      .query({
+        query: GET_LESSEONS_QUERY,
+      })
+      .then((response) => {
+        console.log(response.data);
+      });
+  }, []);
   return <h1 className="text-5xl font-bold text-violet-500  ">Hello world</h1>;
 }
 
